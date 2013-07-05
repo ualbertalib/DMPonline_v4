d452aa2dcc8c78b8bb15ed17ecca42fffd06d4af
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-07-05 10:17:24 +0100

    Revert "Added simple log in"
    This reverts commit 83c59c494c5b2a1da28c0ead8e5269c1ce913436.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627125948) do
+ActiveRecord::Schema.define(:version => 20130625125419) do
 
   create_table "answers", :force => true do |t|
     t.text     "text"
@@ -224,28 +224,17 @@ ActiveRecord::Schema.define(:version => 20130627125948) do
   create_table "users", :force => true do |t|
     t.string   "firstname"
     t.string   "surname"
+    t.string   "email"
+    t.string   "password"
     t.string   "orcid_id"
     t.string   "shibboleth_id"
     t.integer  "user_type_id"
     t.integer  "user_status_id"
     t.integer  "login_count"
     t.datetime "last_login"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "versions", :force => true do |t|
     t.string   "title"
