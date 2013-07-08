2c5aebb5d7cec18db97bd0600b7c363ae7e5d79c
Marta Ribeiro <marta.ribeiro@ed.ac.uk>
2013-07-08 14:07:41 +0100

    Some fixes on the migration files

--- db/migrate/20130625125419_fix_question_theme_relation.rb
+++ db/migrate/20130625125419_fix_question_theme_relation.rb
@@ -1,7 +1,6 @@
 class FixQuestionThemeRelation < ActiveRecord::Migration
   def self.up
-  	drop_table :question_themes
-    create_table :questions_themes, :id => false do |t|
+      create_table :questions_themes, :id => false do |t|
 	  t.references :question, :null => false
 	  t.references :theme, :null => false
 	end
