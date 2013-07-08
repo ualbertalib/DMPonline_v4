bc4d554b9d7ad4e95b1e8d8305efbc2a83caedba
Marta Ribeiro <marta.ribeiro@ed.ac.uk>
2013-07-08 11:28:53 +0100

    Fix a bug - root directive
    Move root directive before ActiveAdmin route.
    Add a folder inside 'views' for all admin interfaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 DMPonline4::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  
+    # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'home#index'
+
   ActiveAdmin.routes(self)
 
   get "home/index"
@@ -84,9 +89,6 @@ DMPonline4::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
