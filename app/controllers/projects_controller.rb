ef796d01d61eca6819308e378c0e2355b1a64a40
Marta Ribeiro <marta.ribeiro@ed.ac.uk>
2013-07-17 10:53:04 +0100

    Project details display small changes
    Small changes made to show.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all(:include => :dmptemplate, :select => "dmptemplates.*, dmptemplates.id as dmptemplate_id")
+    @projects = Project.where("dmptemplate_id IS NOT NULL")
 
     respond_to do |format|
       format.html # index.html.erb
