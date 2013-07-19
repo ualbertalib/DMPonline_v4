a1f150e27b8ce651290bfbaf81b9e21b1e9fdf2f
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-07-19 17:07:20 +0100

    Small fix

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -3,11 +3,10 @@ $( document ).ready(function() {
 	$("form.answer").submit(function(){
 		timeout = setTimeout(function(){
 			$.getJSON("status.json", function(data) {
-				$("#questions-progress").css("width", (data.num_answers/data.num_questions*100)+"%")
+				$("#questions-progress").css("width", (data.num_answers/data.num_questions*100)+"%");
 				$("#questions-progress").text(data.num_answers+" of "+data.num_questions+" answered");
 			});
 		},2000);
-		return false;
 	});
 
 });
