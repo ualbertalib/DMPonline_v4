dcefb45e7c1a9e46952fc33d9c5098828c607a8a
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-07-19 17:00:40 +0100

    Bootstrapped plan edit form inc. accordion and progress bar. Also removed a number of rich-text options.

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -1,16 +1,13 @@
 $( document ).ready(function() {
-
-	$("#sections-accordion").accordion({
-		heightStyle: "content",
-		collapsible: true
-	});
-
-	$.getJSON("status.json", function(data) {
-		$("#questions-progress").progressbar({
-			max: data.num_questions,
-			value: data.num_answers
-		});
-		$(".progress-label").text(data.num_answers+" of "+data.num_questions+" questions answered.");
+	
+	$("form.answer").submit(function(){
+		timeout = setTimeout(function(){
+			$.getJSON("status.json", function(data) {
+				$("#questions-progress").css("width", (data.num_answers/data.num_questions*100)+"%")
+				$("#questions-progress").text(data.num_answers+" of "+data.num_questions+" answered");
+			});
+		},2000);
+		return false;
 	});
 
 });
