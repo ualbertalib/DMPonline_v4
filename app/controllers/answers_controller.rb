2ba7d78629c3792e70fb1c7bbe4a02bd14fd2a93
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-07-26 15:05:48 +0100

    Deleted some unnecessary stuff

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,41 +1,4 @@
 class AnswersController < ApplicationController
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @answers }
-    end
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
-    @answer = Answer.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @answer }
-    end
-  end
-
-  # GET /answers/new
-  # GET /answers/new.json
-  def new
-    @answer = Answer.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @answer }
-    end
-  end
-
-  # GET /answers/1/edit
-  def edit
-    @answer = Answer.find(params[:id])
-  end
 
   # POST /answers
   # POST /answers.json
@@ -65,32 +28,3 @@ class AnswersController < ApplicationController
 		end
 	end
   end
-
-  # PUT /answers/1
-  # PUT /answers/1.json
-  def update
-    @answer = Answer.find(params[:id])
-
-    respond_to do |format|
-      if @answer.update_attributes(params[:answer])
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /answers/1
-  # DELETE /answers/1.json
-  def destroy
-    @answer = Answer.find(params[:id])
-    @answer.destroy
-
-    respond_to do |format|
-      format.html { redirect_to answers_url }
-      format.json { head :no_content }
-    end
-  end
-end
