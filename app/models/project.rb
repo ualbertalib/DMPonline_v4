bdce6cc233fa3ed6cb07772860a5bd9b2d4b83cb
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-08-28 11:52:34 +0100

    Slightly more elegant implementation of setting unit_it for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 
 	extend FriendlyId
 
-	attr_accessible :dmptemplate_id, :locked, :note, :title, :organisation_id
+	attr_accessible :dmptemplate_id, :locked, :note, :title, :organisation_id, :unit_id
 
 	#associations between tables
 	belongs_to :dmptemplate
@@ -13,12 +13,22 @@ class Project < ActiveRecord::Base
 	friendly_id :title, use: :slugged
 	
 	def organisation_name
-	 if dmptemplate.nil?
-     return false
-   else
-    return organisation
-   end 
+		if dmptemplate.nil?
+			return false
+		else
+			return organisation
+		end
 	end	
 
+	def unit_id=(new_unit_id)
+		organisation_id = new_unit_id    
+	end
 	
+	def unit_id
+		if organisation.nil? || organisation.parent_id.nil?
+			return nil
+		else
+			return organisation_id
+		end
+	end
 end
