8fdccf3c8fcf47b83e5231b10acdc628787ce1b2
Marta Ribeiro <marta.ribeiro@ed.ac.uk>
2013-08-30 10:33:58 +0100

    some css

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ DMPonline4::Application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  get "home/index"
-  get "about_us" => 'static_pages#about_us', :as => "about_us"
+   get "about_us" => 'static_pages#about_us', :as => "about_us"
   get "contact_us" => 'static_pages#contact_us', :as => "contact_us"
  
   resources :dmptemplates
