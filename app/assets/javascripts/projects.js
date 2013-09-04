3ffa946f8b9cfb3b9b8a60f0498e5cf49f3566f6
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-09-04 21:48:42 +0100

    Confirmation dialog on project creation
    Bug on project creation related to the creation of plans and the
    FriendlyId gem. May need to rethink URL scheme.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,11 +1,63 @@
 $( document ).ready(function() {
-	
+
 	$("#project_institution_id").change(function (i) {
-		update_child_options($("#project_unit_id"), "children", i.val, 1)
+		update_child_options($("#project_unit_id"), "children", i.val, 1);
+		$("#confirm-institution").text($("#project_institution_id :selected").text());
+		$("#confirm-unit").text("");
 	});
 	
 	$("#project_funder_id").change(function (f) {
-		update_child_options($("#project_dmptemplate_id"), "templates", f.val, 2)
+		update_child_options($("#project_dmptemplate_id"), "templates", f.val, 2);
+		$("#confirm-funder").text($("#project_funder_id :selected").text());
+		$("#confirm-template").text("");
+	});
+	
+	$("#project_dmptemplate_id").change(function (f) {
+		$("#confirm-template").text($("#project_dmptemplate_id :selected").text());
+	});
+	
+	$("#project_unit_id").change(function (f) {
+		$("#confirm-unit").text($("#project_unit_id :selected").text());
+	});
+	
+	$(":checkbox").change(function(){
+		if ($(this).is(":checked")) {
+			$("#confirm-guidance").append("<li id='confirm-"+$(this).attr("id")+"'>"+$(this).parent().text()+"</li>");
+		}
+		else {
+			$("#confirm-"+$(this).attr("id")).remove();
+		}
+	});
+	
+	$("#project-confirmation-dialog").on("show", function(){
+		if ($("#confirm-institution").text() == "") {
+			$("#confirm-institution").text("None");
+		}
+		if ($("#confirm-funder").text() == "") {
+			$("#confirm-funder").text("None");
+		}
+		if ($("#confirm-template").text() == "") {
+			$("#confirm-template").closest("div").hide();
+		}
+		else {
+			$("#confirm-template").closest("div").show();
+		}
+		if ($("#confirm-unit").text() == "") {
+			$("#confirm-unit").closest("div").hide();
+		}
+		else {
+			$("#confirm-unit").closest("div").show();
+		}
+		$('.select2-choice').hide();
+	});
+	
+	$("#new-project-cancelled").click(function (){
+		$("#project-confirmation-dialog").modal("hide");
+		$('.select2-choice').show();
+	});
+	
+	$("#new-project-confirmed").click(function (){
+		$("#new_project").submit();
 	});
 	
 	function update_child_options(select_element, action, parent_id, min_to_display) {
