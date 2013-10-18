db418d3a078688ddb7e9e226871e05cad3ffabc2
Marta Ribeiro <marta.ribeiro@ed.ac.uk>
2013-10-18 12:59:55 +0100

    Remove Captcha when logged in

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,30 @@
 class ContactsController < ContactUs::ContactsController	
 	def create
 		@contact = ContactUs::Contact.new(params[:contact_us_contact])
-		if verify_recaptcha(:message => "You have not added the validation words correctly") && @contact.save
-			flash[:notice] = t('contact_us.notices.success')
-			if user_signed_in? then
-		  		redirect_to :controller => 'projects', :action => 'index'
-		  	else
-		  		redirect_to(root_path)
-		  	end
+		if (!user_signed_in?)
+			if verify_recaptcha(:message => "You have not added the validation words correctly") && @contact.save
+				flash[:notice] = t('contact_us.notices.success')
+				if user_signed_in? then
+			  		redirect_to :controller => 'projects', :action => 'index'
+			  	else
+			  		redirect_to(root_path)
+			  	end
+			else
+			  	flash[:error] = t('contact_us.notices.error')
+			  	render_new_page
+			end
 		else
-		  	flash[:error] = t('contact_us.notices.error')
-		  	render_new_page
-		end
+			if @contact.save
+				flash[:notice] = t('contact_us.notices.success')
+				if user_signed_in? then
+			  		redirect_to :controller => 'projects', :action => 'index'
+			  	else
+			  		redirect_to(root_path)
+			  	end
+			else
+			  	flash[:error] = t('contact_us.notices.error')
+			  	render_new_page
+			end
+		end		
 	end
 end
