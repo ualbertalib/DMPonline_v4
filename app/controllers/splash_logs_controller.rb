31ba28331b51f7cd3216ee6af57d8548fae7d76c
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-11-27 15:14:12 +0000

    Setting/reading cookie to avoid constant bombardment by splash dialog. Link to DMPonline3 for appropriate users.

--- app/controllers/splash_logs_controller.rb
+++ app/controllers/splash_logs_controller.rb
@@ -7,6 +7,10 @@ class SplashLogsController < ApplicationController
 		@splash_log.destination = params[:destination]
 		respond_to do |format|
 			if @splash_log.save
+				cookies[:dmp_splash_seen] = {
+					value: 'splash_dialog_seen',
+					expires: 3.hours.from_now,
+				}
 				format.html { redirect_to params[:destination] }
 			else
 				format.html { redirect_to home_url }
