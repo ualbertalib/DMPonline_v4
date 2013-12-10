4ccea1a7f47e88f0345dd075aa002a773052fc35
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2013-12-10 14:48:00 +0000

    Fix for news page not displaying

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,6 +11,7 @@ class StaticPagesController < ApplicationController
   	@dcc_news_feed = Feedzirra::Feed.fetch_and_parse(dcc_news_feed_url)
   	respond_to do |format|
   		format.rss { redirect_to dcc_news_feed_url }
+  		format.html
   	end
   end
 end
