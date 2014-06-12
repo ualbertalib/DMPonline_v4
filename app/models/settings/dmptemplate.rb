11e80cb9fdaccc4a47cf6006b909e0b346586c13
uoldmp <uoldmp@users.noreply.github.com>
2014-06-12 10:11:19 +0100

    Tests and extra validation regarding template settings

--- app/models/settings/dmptemplate.rb
+++ app/models/settings/dmptemplate.rb
@@ -1,6 +1,10 @@
 module Settings
   class Dmptemplate < RailsSettings::SettingObject
 
+    VALID_FONT_FACES = [
+      'Arial, Helvetica, Sans-Serif'
+    ]
+
     DEFAULT_FORMATTING = {
       margin: { # in millimeters
         top:    20,
@@ -8,23 +12,50 @@ module Settings
         left:   20,
         right:  20
       },
-      font_face: 'Arial, Helvetica, Sans-Serif',
+      font_face: VALID_FONT_FACES.first,
       font_size: 12 # pt
     }
+
+    class << self
+      def default_formatting_for(project)
+        if project.dmptemplate.present?
+          project.dmptemplate.settings(:export).formatting
+        else
+          DEFAULT_FORMATTING
+        end
+      end
+    end
 
     validate do
       formatting = value['formatting']
 
       if formatting.present?
         errs = []
-        errs << :negative_margin if formatting[:margin].any? {|m| m.to_i < 0 }
-        errs << :negative_font_size if formatting[:font_size].to_i < 0
-        errs << :unkown_margin unless (formatting.margin.keys - DEFAULT_FORMATTING.margin.keys).empty?
-        errs << :unknown_key unless (formatting.keys - DEFAULT_FORMATTING.keys).empty?
+
+        unless (DEFAULT_FORMATTING.keys - formatting.keys).empty?
+          errs << :missing_key
+        else
+          unless formatting[:margin].is_a?(Hash)
+            errs << :invalid_margin
+          else
+            errs << :negative_margin if formatting[:margin].any? {|k,v| v.to_i < 0 }
+            errs << :unknown_margin unless (formatting[:margin].keys - DEFAULT_FORMATTING[:margin].keys).empty?
+          end
+
+          if !formatting[:font_size].is_a?(Integer)
+            errs << :invalid_font_size
+          elsif formatting[:font_size].to_i < 0
+            errs << :negative_font_size
+          end
+
+          errs << :invalid_font_face unless VALID_FONT_FACES.member?(formatting[:font_face])
+          errs << :unknown_key unless (formatting.keys - DEFAULT_FORMATTING.keys).empty?
+        end
 
         errs.map do |key|
           errors.add(:formatting, I18n.t("helpers.settings.templates.errors.#{key}"))
         end
+
       end
     end
   end
