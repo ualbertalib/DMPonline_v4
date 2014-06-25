ef14d4033c2693da43a08a4562484cc87844d1dd
uoldmp <uoldmp@users.noreply.github.com>
2014-06-25 12:04:50 +0100

    Don't include toggle in checks and set toggle checked attr

--- app/assets/javascripts/export_configure.js
+++ app/assets/javascripts/export_configure.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
 
   $('.check_select').each(function() {
     var toggle = $('<input type="checkbox" />').appendTo($(this).find('legend')),
-        checks = $(this).find('input[type=checkbox]'),
+        checks = $(this).find('li input[type=checkbox]'),
        checked = checks.length == checks.filter(':checked').length;
+
+    toggle.prop('checked', checked);
 
     toggle.click(function() {
       checks.prop('checked', toggle.is(':checked'));
