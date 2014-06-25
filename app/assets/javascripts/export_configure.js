24900ef6ec4c9d602fe3e07a8b73c147829e9d48
uoldmp <uoldmp@users.noreply.github.com>
2014-06-25 09:53:41 +0100

    Allow multiple checkbox toggles on settings form

--- app/assets/javascripts/export_configure.js
+++ app/assets/javascripts/export_configure.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-  var admin_details_all = $('<input type="checkbox" />').appendTo('#admin_details legend'),
-           admin_checks = $('#admin_details > input[type=checkbox]'),
-            all_checked = admin_checks.length == admin_checks.filter(':checked').length;
+  $('.check_select').each(function() {
+    var toggle = $('<input type="checkbox" />').appendTo($(this).find('legend')),
+        checks = $(this).find('input[type=checkbox]'),
+       checked = checks.length == checks.filter(':checked').length;
 
-  admin_details_all.attr('checked', all_checked);
-
-  admin_details_all.click(function() {
-    admin_checks.prop('checked', admin_details_all.is(':checked'));
-  })
+    toggle.click(function() {
+      checks.prop('checked', toggle.is(':checked'));
+    });
+  });
 
 });
