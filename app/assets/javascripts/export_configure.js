606298de63772cbbd45e603ec87f2159ec925332
uoldmp <uoldmp@users.noreply.github.com>
2014-06-26 14:25:35 +0100

    Add indeterminate state to section toggles

--- app/assets/javascripts/export_configure.js
+++ app/assets/javascripts/export_configure.js
@@ -6,16 +6,29 @@ $(document).ready(function() {
     var container = $(this),
            toggle = container.find('> legend > .toggle'),
            checks = container.find('li input[type=checkbox]'),
-          checked = function() { return checks.length == checks.filter(':checked').length; };
+          checked = function() {
+            var checked = checks.filter(':checked');
 
-    toggle.prop('checked', checked());
+            if (checks.length == checked.length)
+              return {'checked': true};
+
+            if (checked.length == 0)
+              return {'checked': false};
+
+            return {'indeterminate': true}
+          };
+
+    toggle.prop(checked());
 
     checks.change(function() {
-      toggle.prop('checked', checked());
+      toggle.prop('indeterminate', false);
+      toggle.prop(checked());
     });
 
     toggle.change(function() {
+      toggle.prop('indeterminate', false);
       checks.prop('checked', toggle.is(':checked'));
+      toggle.prop(checked());
     });
 
   });
