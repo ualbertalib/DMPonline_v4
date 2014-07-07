5bf9c4f669f80ec0082cd816fba5c3d5fe0bd01f
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2014-07-07 14:47:15 +0100

    Added "My organisation isn't listed" link for registration
    Clicking hides link, select “other” in select box and displays text
    field for entering name of other org.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require bootstrap
 //= require v1.js
 //= require select2.min.js
-//= require jquery.placeholder.js 
+//= require jquery.placeholder.js
 
 
 $( document ).ready(function() {
@@ -29,44 +29,44 @@ $( document ).ready(function() {
 	}).on('hide', function(){
 		var minus = $(this).parent().children(".accordion-heading").children(".accordion-toggle").children(".icon-minus").removeClass("icon-minus").addClass("icon-plus");
 	});
-	
-	//accordion home page 
+
+	//accordion home page
 	$('.accordion-home').on('show', function() {
 		var plus = $(this).parent().find(".plus-laranja").removeClass("plus-laranja").addClass("minus-laranja");
 	}).on('hide', function(){
 		var minus = $(this).parent().find(".minus-laranja").removeClass("minus-laranja").addClass("plus-laranja");
 	});
-	
+
 	//accordion project details page when project has more than 1 plan
 	$('.accordion-project').on('show', function() {
 		var plus = $(this).parent().find(".plus-laranja").removeClass("plus-laranja").addClass("minus-laranja");
 	}).on('hide', function(){
 		var minus = $(this).parent().find(".minus-laranja").removeClass("minus-laranja").addClass("plus-laranja");
 	});
-	
+
 	//$('#3-or-4-splash').modal();
-		
+
 	$('.typeahead').select2({
 		width: "element",
 		allowClear: true
 	});
-	
+
 	$(".help").popover();
-	
+
 	$('.has-tooltip').tooltip({
     placement: "right",
     trigger: "focus"
 	});
-	
+
 	$(".show-edit-toggle").click(function () {
 		$(".edit-project").toggle();
 		$(".view-project").toggle();
 	});
-	
+
 	$(".toggle-existing-user-access").change(function(){
 		$(this).closest("form").submit();
 	});
-	
+
 	$("#user_email.text_field.reg-input").blur(function () {
 		if (validateEmail($(this).val())) {
 			$(this).parent().children("div").hide();
@@ -75,7 +75,7 @@ $( document ).ready(function() {
 			$(this).parent().children("div").show();
 		}
 	});
-	
+
 	$("#user_password.text_field.reg-input").blur(function () {
 		if ($(this).val().length >= 8) {
 			$(this).parent().children("div").hide();
@@ -84,7 +84,7 @@ $( document ).ready(function() {
 			$(this).parent().children("div").show();
 		}
 	});
-	
+
 	$("#user_password_confirmation.text_field.reg-input").blur(function () {
 		if ($(this).val() == $("#user_password.text_field.reg-input").val()) {
 			$(this).parent().children("div").hide();
@@ -93,19 +93,29 @@ $( document ).ready(function() {
 			$(this).parent().children("div").show();
 		}
 	});
-	
+
 	$('#user_organisation_id').on("change", function(e) {
+		e.preventDefault();
 		var selected_org = $(this).select2("val");
 		var other_orgs = $("#other-organisation-name").attr("data-orgs").split(",");
 		var index = $.inArray(selected_org, other_orgs);
 		if (index > -1) {
 			$("#other-organisation-name").show();
+			$("#user_other_organisation").focus();
 		}
 		else {
 			$("#other-organisation-name").hide();
 		}
 	});
-	
+
+	$("#other-org-link > a").click(function(e){
+		e.preventDefault();
+		var other_org = $("#other-organisation-name").attr("data-orgs").split(",");
+		$("#user_organisation_id").select2("val", other_org);
+		$("#other-org-link").hide();
+		$("#user_organisation_id").change();
+	});
+
 	/*$('#continue-to-new').click(function(e){
 		var destination = $(this).attr("href");
 		var n = destination.lastIndexOf('=');
