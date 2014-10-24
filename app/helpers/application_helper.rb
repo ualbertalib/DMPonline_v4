ualbertalib/DMPonline_v4

commit 741624a1546094750a92de90f60add002e0f7928

Number of small fixes

app/helpers/application_helper.rb
@@ -15,9 +15,13 @@ module ApplicationHelper
 	  content_for(:head) { javascript_include_tag(*files) }
 	end
 	
-	def link_to_add_object(name, f, association, css_class)
+    
+	def link_to_add_object(name, f, association, css_class, i)
         new_object = f.object.class.reflect_on_association(association).klass.new
+        j = i
         fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+          j = j + 1
+          new_object.number = j
           render(association.to_s.singularize + "_fields", :f => builder)
         end
         link_to_function(name, "add_object(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => css_class)

app/admin/dmptemplate.rb
@@ -1,61 +1,60 @@
 ActiveAdmin.register Dmptemplate do
 	
-	 menu :priority => 11, :label => proc{ I18n.t('admin.template')}, :parent => "Templates management"
+	menu :priority => 11, :label => proc{ I18n.t('admin.template')}, :parent => "Templates management"
 
-  # FIXME: The below member_actions only work on :export settings.
-  member_action :settings do
-    @template = resource
-    @settings = resource.settings(:export)
-  end
-
-  member_action :update_settings, method: :put do
-    new_settings = params[:settings][:export]
-
-    settings = resource.settings(:export).tap do |s|
-      s.formatting, s.max_pages = if params[:commit] != 'Reset'
-        [
-          new_settings[:formatting].try(:deep_symbolize_keys),
-          new_settings[:max_pages].try(:to_i)
-        ]
-      else
-        [ nil, nil ]
-      end
+    # FIXME: The below member_actions only work on :export settings.
+    member_action :settings do
+        @template = resource
+        @settings = resource.settings(:export)
     end
 
-    if settings.save
-      redirect_to(action: :show, flash: { notice: 'Settings updated successfully' })
-    else
-      settings.formatting = nil
-      @template = resource
-      @settings = settings
-      render(action: :settings)
-    end
-  end
+    member_action :update_settings, method: :put do
+        new_settings = params[:settings][:export]
 
-  action_item only: %i( show edit ) do
-    link_to(I18n.t('helpers.settings.title'), settings_admin_dmptemplate_path(resource.id))
-  end
-	
-  index do   
-  	column :title do |dmptemp|
-        link_to dmptemp.title, [:admin, dmptemp]
+        settings = resource.settings(:export).tap do |s|
+            s.formatting, s.max_pages = if params[:commit] != 'Reset'
+            [
+                new_settings[:formatting].try(:deep_symbolize_keys),
+                new_settings[:max_pages].try(:to_i)
+            ]
+            else
+                [ nil, nil ]
+            end
+        end
+
+        if settings.save
+            redirect_to(action: :show, flash: { notice: 'Settings updated successfully' })
+        else
+            settings.formatting = nil
+            @template = resource
+            @settings = settings
+            render(action: :settings)
+        end
     end
-  	column :description do |descr|
-  		if !descr.description.nil? then
-  			descr.description.html_safe
-  		end
-  		
-  	end
-  	column I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
-        link_to org_title.organisation.name, [:admin, org_title.organisation]
-   	end
-  	column :published
-  	column :is_default
-  	
-    actions defaults: true do |template|
-      link_to(I18n.t('helpers.settings.title'), settings_admin_dmptemplate_path(template.id))
+
+    action_item only: %i( show edit ) do
+        link_to(I18n.t('helpers.settings.title'), settings_admin_dmptemplate_path(resource.id))
     end
-  end
+
+    index do   
+        column :title do |dmptemp|
+            link_to dmptemp.title, [:admin, dmptemp]
+        end
+        column :description do |descr|
+            if !descr.description.nil? then
+                descr.description.html_safe
+            end
+        end
+        column I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
+            link_to org_title.organisation.name, [:admin, org_title.organisation]
+        end
+        column :published
+        column :is_default
+        
+        actions defaults: true do |template|
+            link_to(I18n.t('helpers.settings.title'), settings_admin_dmptemplate_path(template.id))
+        end
+    end
  
  
 	# show Template details
@@ -63,44 +62,43 @@ ActiveAdmin.register Dmptemplate do
 		attributes_table do
 			row :title
 	 		row :description do |descr|
-	  		if !descr.description.nil? then
-	  			descr.description.html_safe
-	  		end
-	  	end
-	  	row I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
-        link_to org_title.organisation.name, [:admin, org_title.organisation]
-   		end
-	 		row :published
-	 		row :is_default
-	 		row :created_at
-     	row :updated_at
-		 end
-		end 
+                if !descr.description.nil? then
+                    descr.description.html_safe
+                end
+            end
+            row I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
+                link_to org_title.organisation.name, [:admin, org_title.organisation]
+            end
+            row :published
+            row :is_default
+            row :created_at
+            row :updated_at
+        end
+	end 
 		 
-		#phases sidebar
- 		sidebar I18n.t('admin.phases'), :only => :show, :if => proc { dmptemplate.phases.count >= 1} do
- 		 	table_for dmptemplate.phases.order("number asc") do |temp_phases|
- 		 		column :number
- 		 		column :title do |row|
-      		link_to row.title, [:admin, row]
-      	end	
-      	
- 		 	end
- 		end
+    #phases sidebar
+    sidebar I18n.t('admin.phases'), :only => :show, :if => proc { dmptemplate.phases.count >= 1} do
+        table_for dmptemplate.phases.order("number asc") do |temp_phases|
+            column :number
+            column :title do |row|
+                link_to row.title, [:admin, row]
+            end	
+        end
+    end
 			 
  	#form 	
  	form do |f|
-  	f.inputs "Details" do
-  		f.input :title
-  		f.input :description
-  		f.input :organisation_id, :label => I18n.t('admin.org_title'), 
-  						:as => :select, 
-  						:collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
-  		f.input :published
-  		f.input :is_default
-  	end
-  	f.actions  
-  end		
+        f.inputs "Details" do
+            f.input :title
+            f.input :description
+            f.input :organisation_id, :label => I18n.t('admin.org_title'), 
+                            :as => :select, 
+                            :collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
+            f.input :published
+            f.input :is_default
+        end
+        f.actions  
+    end		
  
 end
 

app/admin/guidance.rb
@@ -2,20 +2,20 @@ ActiveAdmin.register Guidance do
 
 	menu :priority => 13, :label => proc{ I18n.t('admin.guidance')}, :parent => "Guidance list"
 	 
-	  index do   #:file_id, :text, :guidance_group_id, :theme_id, :question_id
+	index do   #:file_id, :text, :guidance_group_id, :theme_id, :question_id
   		column (:text) { |guidance| raw(guidance.text) }
   		column I18n.t('admin.theme') do |t|
-	 		 	(t.themes.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
-	 		end	  
-	 		column I18n.t('admin.old_theme_field'), :sortable => :theme_id do |t|
-	 		 	t.theme
-	 		end	 	 		
-	 		column I18n.t('admin.question'),  :sortable => :question_id do |que|
-	    	if !que.nil? then
-   				que.question
-   			else
-   				'-'	
-   			end	
+	 		 (t.themes.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
+        end	  
+        column I18n.t('admin.old_theme_field'), :sortable => :theme_id do |t|
+            t.theme
+        end	 	 		
+        column I18n.t('admin.question'),  :sortable => :question_id do |que|
+            if !que.nil? then
+                que.question
+            else
+                '-'	
+            end	
 	    end
 	    column I18n.t('admin.old_temp_field'), :sortable => :dmptemplate_id do |temp| 
 	    	if !temp.nil? then
@@ -27,60 +27,60 @@ ActiveAdmin.register Guidance do
 	    column I18n.t('admin.guidance_group') do |guidance| 
 	    	(guidance.guidance_groups.map{|t_q| link_to t_q.name, [:admin, t_q]}).join(', ').html_safe
 	    end  	
-    default_actions
-  end
+        default_actions
+    end
   
-  #show details of a question
+    #show details of a question
 	show do
 		attributes_table do
 			row (:text) { |guidance| raw(guidance.text) }
 			row I18n.t('admin.old_theme_field'), :theme_id do |guidance|
-   			guidance.theme
-	    end
-	    row I18n.t('admin.theme') do
-	 		 	(guidance.themes.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
-	 		end	
-	    row I18n.t('admin.question'), :question_id do |question|
-   			question.question
-	    end
-	    row I18n.t('admin.guidance_group') do |guidance| 
-	 		 	(guidance.guidance_groups.map{|t_q| link_to t_q.name, [:admin, t_q]}).join(', ').html_safe
-	 		end	
-	    row I18n.t('admin.old_temp_field'), :dmptemplate do |temp|
-	    	if !temp.dmptemplate.nil? then
-	    		link_to temp.dmptemplate.title, [:admin, temp.dmptemplate]
-	    	end
-	    end  	
-	    row :created_at
-	    row :updated_at
+                guidance.theme
+            end
+            row I18n.t('admin.theme') do
+                (guidance.themes.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
+            end	
+            row I18n.t('admin.question'), :question_id do |question|
+                question.question
+            end
+            row I18n.t('admin.guidance_group') do |guidance| 
+                (guidance.guidance_groups.map{|t_q| link_to t_q.name, [:admin, t_q]}).join(', ').html_safe
+            end	
+            row I18n.t('admin.old_temp_field'), :dmptemplate do |temp|
+                if !temp.dmptemplate.nil? then
+                    link_to temp.dmptemplate.title, [:admin, temp.dmptemplate]
+                end
+            end  	
+            row :created_at
+            row :updated_at
 		end
 	end
 
 	#form 
-  form do |f|
-  	f.inputs "Details" do
-  		f.input :text
-  		f.input :question_id, :as => :select, 
-  						:collection => Question.find(:all, :order => 'text ASC').map{|que|[que.text, que.id]}
-  		f.input :guidance_group_ids, :label => I18n.t('admin.guidance_group'), 
-  						:as => :select, 
-  						:collection => GuidanceGroup.find(:all, :order => 'name ASC').map{|gui|[gui.name, gui.id]}
-    	f.input :theme, :as => :select, 
-  						:label =>'Old theme field',
-  						:collection => Theme.find(:all, :order => 'title ASC').map{|the| [the.title, the.id]}		
-  			
-		end
-		f.inputs "Themes" do
-  			f.input :theme_ids, :label => "Selected themes",
-  							:as => :select, 
-  							:include_blank => "All themes", 
-  							:multiple => true,
-  							:collection => Theme.order('title').map{|the| [the.title, the.id]},
-  							:hint => 'Choose all themes that apply.'	
-  	end
-		
-		 f.actions  
-	end			
+    form do |f|
+        f.inputs "Details" do
+            f.input :text
+            f.input :question_id, :as => :select, 
+                    :collection => Question.find(:all, :order => 'text ASC').map{|que|[que.text, que.id]}
+            f.input :guidance_group_ids, :label => I18n.t('admin.guidance_group'), 
+                    :as => :select, 
+                    :collection => GuidanceGroup.find(:all, :order => 'name ASC').map{|gui|[gui.name, gui.id]}
+            f.input :theme, :as => :select, 
+                    :label =>'Old theme field',
+                    :collection => Theme.find(:all, :order => 'title ASC').map{|the| [the.title, the.id]}		
+                
+        end
+        f.inputs "Themes" do
+            f.input :theme_ids, :label => "Selected themes",
+                    :as => :select, 
+                    :include_blank => "All themes", 
+                    :multiple => true,
+                    :collection => Theme.order('title').map{|the| [the.title, the.id]},
+                    :hint => 'Choose all themes that apply.'	
+        end
+        
+        f.actions  
+    end			
 	
 	
 end

app/admin/guidance_group.rb
@@ -3,64 +3,64 @@ ActiveAdmin.register GuidanceGroup do
 	menu :priority => 2, :label => proc{I18n.t('admin.guidance_group')}, :parent => "Guidance list"
 	
 	index do   #:organisation_id, :name
-  	column I18n.t('admin.name'), :sortable => :name do |ggn|
-        link_to ggn.name, [:admin, ggn]
+        column I18n.t('admin.name'), :sortable => :name do |ggn|
+            link_to ggn.name, [:admin, ggn]
+        end
+        column I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
+            link_to org_title.organisation.name, [:admin, org_title.organisation]
+        end
+        column I18n.t('admin.template') do |t|
+            (t.dmptemplates.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
+        end	
+        
+        default_actions
     end
-    column I18n.t('admin.org_title'), :sortable => :organisation_id do |org_title|
-        link_to org_title.organisation.name, [:admin, org_title.organisation]
-    end
-    column I18n.t('admin.template') do |t|
-	 		 	(t.dmptemplates.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
-	 	end	
-  	
-  	default_actions
-  end
   
-  #show details of guidance group
-  show do
+    #show details of guidance group
+    show do
 		attributes_table do
 			row	:name
 			row :organisation_id do |org_title|
-        link_to org_title.organisation.name, [:admin, org_title.organisation]
-    	end
-    	row I18n.t('admin.template') do
+                link_to org_title.organisation.name, [:admin, org_title.organisation]
+            end
+            row I18n.t('admin.template') do
 	 		 	(guidance_group.dmptemplates.map{|t_q| link_to t_q.title, [:admin, t_q]}).join(', ').html_safe
-	 		end	
-    	row :created_at
-    	row :updated_at
+            end	
+            row :created_at
+            row :updated_at
 		end
 	end
 	
 	#guidance list
 	sidebar I18n.t('admin.guidance'), :only => :show, :if => proc { guidance_group.guidances.count >= 1}  do
- 		 	table_for guidance_group.guidances.order("text asc") do |guis|
- 		 		column :text do |gtext|
- 		 			link_to gtext.text.html_safe, [:admin, gtext]
- 		 		end
- 		 		column I18n.t('admin.theme'), :theme_id do |themelist|
-   				themelist.theme
-	    	end
- 		 	end
- 		end
+        table_for guidance_group.guidances.order("text asc") do |guis|
+            column :text do |gtext|
+                link_to gtext.text.html_safe, [:admin, gtext]
+            end
+            column I18n.t('admin.theme'), :theme_id do |themelist|
+                themelist.theme
+            end
+        end
+    end
 	
 	#form 	
  	form do |f|
-  	f.inputs "Details" do
-  		f.input :name
-			f.input :organisation_id, :label => I18n.t('admin.org_title'), 
-							:as => :select, 
-							:collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
+        f.inputs "Details" do
+            f.input :name
+            f.input :organisation_id, :label => I18n.t('admin.org_title'), 
+                    :as => :select, 
+                    :collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
 		end
 		
-  	f.inputs "Templates" do
+        f.inputs "Templates" do
   			f.input :dmptemplate_ids,  :label => "Selected templates",
-  							:as => :select, 
-  							:include_blank => "All Templates", 
-  							:multiple => true,
-  							:collection => Dmptemplate.order('title').map{|the| [the.title, the.id]},
-  							:hint => 'Choose all templates that apply.'
-  	end
-  	f.actions  
-  end		
+                    :as => :select, 
+                    :include_blank => "All Templates", 
+                    :multiple => true,
+                    :collection => Dmptemplate.order('title').map{|the| [the.title, the.id]},
+                    :hint => 'Choose all templates that apply.'
+        end
+        f.actions  
+    end		
   		
 end

app/admin/option.rb
@@ -2,28 +2,28 @@ ActiveAdmin.register Option do
 	menu :priority => 1, :label => proc{I18n.t('admin.multi_options')}, :parent =>  "Templates management"
 
 	index do   # :question_id, :text, :number, :is_default 
-  	column :text
-  	column I18n.t('admin.questions'), :sortable => :question_id do |ques|
-  		if !ques.question_id.nil? then
-        link_to ques.question.text, [:admin, ques.question]
-      end  
+        column :text
+        column I18n.t('admin.questions'), :sortable => :question_id do |ques|
+            if !ques.question_id.nil? then
+                link_to ques.question.text, [:admin, ques.question]
+            end  
+        end
+        column I18n.t('admin.sections'), :sortable => :question_id do |ques|
+            if !ques.question_id.nil? then
+                link_to ques.question.section.title, [:admin, ques.question.section]
+            end
+        end
+        column I18n.t('admin.template'), :sortable => :question_id do |ques|
+            if !ques.question_id.nil? then
+                link_to ques.question.section.version.phase.dmptemplate.title, [:admin, ques.question.section.version.phase.dmptemplate]
+            end  
+        end
+        
+        default_actions
     end
-    column I18n.t('admin.sections'), :sortable => :question_id do |ques|
-    	if !ques.question_id.nil? then
-        link_to ques.question.section.title, [:admin, ques.question.section]
-      end
-    end
-  	column I18n.t('admin.sections'), :sortable => :question_id do |ques|
-  		if !ques.question_id.nil? then
-        link_to ques.question.section.version.phase.dmptemplate.title, [:admin, ques.question.section.version.phase.dmptemplate]
-      end  
-    end
-  	
-  	default_actions
-  end
   
-  #show details of a section
-  show do 
+    #show details of a section
+    show do 
 		attributes_table do
 			row :text
 	 		row	:number
@@ -31,35 +31,35 @@ ActiveAdmin.register Option do
 	 			if !ques.question_id.nil? then
 	 				 link_to ques.question.text, [:admin, ques.question]
 	 			end
-	    end
-	    row I18n.t('admin.sections'), :question_id do |ques|
-	    	if !ques.question_id.nil? then
-	    		link_to ques.question.section.title, [:admin, ques.question.section]
-	    	end
-	    end
-	  	row I18n.t('admin.sections'), :question_id do |ques|
-	  		if !ques.question_id.nil? then
-	  			 link_to ques.question.section.version.phase.dmptemplate.title, [:admin, ques.question.section.version.phase.dmptemplate]
-	  		end
-	    end
-  		row :is_default
-     	row :created_at
-     	row :updated_at
-		 end
-  end
+            end
+            row I18n.t('admin.sections'), :question_id do |ques|
+                if !ques.question_id.nil? then
+                    link_to ques.question.section.title, [:admin, ques.question.section]
+                end
+            end
+            row I18n.t('admin.template'), :question_id do |ques|
+                if !ques.question_id.nil? then
+                     link_to ques.question.section.version.phase.dmptemplate.title, [:admin, ques.question.section.version.phase.dmptemplate]
+                end
+            end
+            row :is_default
+            row :created_at
+            row :updated_at
+		end
+    end
   
   
-  #form 
-  form do |f|
-  	f.inputs "Details" do
-  		f.input :text
-  		f.input :number
-  		f.input :question, 
-  						:as => :select, 
-  						:collection => Question.find(:all, :order => 'text ASC').map{ |sec| ["#{truncate(sec.section.version.phase.dmptemplate.title, :lengh => 20)} - #{truncate(sec.section.title, :lengh => 50)} - #{truncate(sec.text, :lengh => 20)}", sec.id] }
-  		f.input :is_default
-   	end
+    #form 
+    form do |f|
+        f.inputs "Details" do
+            f.input :text
+            f.input :number
+            f.input :question, 
+                    :as => :select, 
+                    :collection => Question.find(:all, :order => 'text ASC').map{ |sec| ["#{truncate(sec.section.version.phase.dmptemplate.title, :lengh => 20)} - #{truncate(sec.section.title, :lengh => 50)} - #{truncate(sec.text, :lengh => 20)}", sec.id] }
+            f.input :is_default
+        end
   	
-  	 f.actions 
-  end
+        f.actions 
+    end
 end

app/admin/organisation.rb
@@ -4,66 +4,66 @@ ActiveAdmin.register Organisation do
 
 	index do   # :abbreviation, :banner_file_id, :description, :domain, :logo_file_id, :name, 
 		#:stylesheet_file_id, :target_url, :organisation_type_id, :wayfless_entity, :parent_id
-  	column I18n.t('admin.org_title'), :sortable => :name do |ggn|
-        link_to ggn.name, [:admin, ggn]
-    end
-    column I18n.t('admin.abbrev'), :sortable => :abbreviation do |ggn|
-        if !ggn.abbreviation.nil?
-        	link_to ggn.abbreviation, [:admin, ggn]
-        else
-        	'-'
+        column I18n.t('admin.org_title'), :sortable => :name do |ggn|
+            link_to ggn.name, [:admin, ggn]
         end
+        column I18n.t('admin.abbrev'), :sortable => :abbreviation do |ggn|
+            if !ggn.abbreviation.nil?
+                link_to ggn.abbreviation, [:admin, ggn]
+            else
+                '-'
+            end
+        end
+        column I18n.t('admin.org_type'), :sortable => :organisation_type_id do |org_type|
+            if !org_type.organisation_type_id.nil? then
+                link_to org_type.organisation_type.name, [:admin, org_type]
+            end  
+        end
+        
+        default_actions
     end
-    column I18n.t('admin.org_type'), :sortable => :organisation_type_id do |org_type|
-    	if !org_type.organisation_type_id.nil? then
-        link_to org_type.organisation_type.name, [:admin, org_type]
-      end  
-    end
-  	
-  	default_actions
-  end
   
   
-  #show details of an organisation
-  show do 
+    #show details of an organisation
+    show do 
 		attributes_table do
 			row I18n.t('admin.org_title'), :sortable => :name do |gn|
 				if !gn.name.nil? then
-        	link_to gn.name, [:admin, gn]
-        end
-    	end
+                    link_to gn.name, [:admin, gn]
+                end
+            end
 			row I18n.t('admin.abbrev'), :abbreviation do |ggn|
-        if !ggn.abbreviation.nil?
-        	link_to ggn.abbreviation, [:admin, ggn]
-        else
-        	'-'
-        end
+                if !ggn.abbreviation.nil?
+                    link_to ggn.abbreviation, [:admin, ggn]
+                else
+                    '-'
+                end
 			end
 			row :sort_name 
 			row I18n.t('admin.org_type'), :organisation_type_id do |org_type|
 				if !org_type.organisation_type_id.nil? then
-        	link_to org_type.organisation_type.name, [:admin, org_type]
-        end	
-      end  
-      row :description do |descr|
-	  		if !descr.description.nil? then
-	  			descr.description.html_safe
-	  		end
-      end
-     	row :target_url
-      row :domain
-      row :wayfless_entity
-      row I18n.t('admin.org_parent'), :parent_id do |org_parent|
-      	if !org_parent.parent_id.nil? then
-      		parent_org = Organisation.find(org_parent.parent_id)
-      		link_to parent_org.name, [:admin, parent_org]
-      	end	
-      end
-    	row :logo_file_id
-     	row :banner_file_id
-    	row :stylesheet_file_id
-    	row :created_at
-    	row :updated_at
+                    link_to org_type.organisation_type.name, [:admin, org_type]
+                end	
+            end  
+            row :description do |descr|
+                if !descr.description.nil? then
+                    descr.description.html_safe
+                end
+            end
+            row :target_url
+            row :domain
+            row :wayfless_entity
+            row I18n.t('admin.org_parent'), :parent_id do |org_parent|
+                if !org_parent.parent_id.nil? then
+                    parent_org = Organisation.find(org_parent.parent_id)
+                    link_to parent_org.name, [:admin, parent_org]
+                end	
+            end
+            row :logo_file_id
+            row :banner_file_id
+            row :stylesheet_file_id
+            row :created_at
+            row :updated_at
 		end
 	end		
 	
@@ -71,30 +71,30 @@ ActiveAdmin.register Organisation do
  	sidebar I18n.t('admin.templates'), :only => :show, :if => proc { organisation.dmptemplates.count >= 1} do
 	 	table_for organisation.dmptemplates.order("title asc") do |temp|
 	 		column :title do |dmptemp|
-     		link_to dmptemp.title, [:admin, dmptemp]
-  		end
-    	column :published
+                link_to dmptemp.title, [:admin, dmptemp]
+            end
+            column :published
 	 	end
 	end
 
 	#form 
-  form do |f|
-  	f.inputs "Details" do
-  		f.input :name
-  		f.input :abbreviation
-  		f.input :sort_name
-  		f.input :description
-  		f.input :organisation_type_id, :label => I18n.t('admin.org_type'), :as => :select, :collection => OrganisationType.find(:all, :order => 'name ASC').map{|orgt|[orgt.name, orgt.id]}
-  		f.input :target_url
-      f.input :domain
-      f.input :wayfless_entity
-      f.input :parent_id, :label => I18n.t('admin.org_parent'), :as => :select, :collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
-      f.input :logo_file_id
-     	f.input :banner_file_id
-    	f.input :stylesheet_file_id
-  		end
-		 f.actions  
-	end			
+    form do |f|
+        f.inputs "Details" do
+            f.input :name
+            f.input :abbreviation
+            f.input :sort_name
+            f.input :description
+            f.input :organisation_type_id, :label => I18n.t('admin.org_type'), :as => :select, :collection => OrganisationType.find(:all, :order => 'name ASC').map{|orgt|[orgt.name, orgt.id]}
+            f.input :target_url
+            f.input :domain
+            f.input :wayfless_entity
+            f.input :parent_id, :label => I18n.t('admin.org_parent'), :as => :select, :collection => Organisation.find(:all, :order => 'name ASC').map{|orgp|[orgp.name, orgp.id]}
+            f.input :logo_file_id
+            f.input :banner_file_id
+            f.input :stylesheet_file_id
+        end
+        f.actions  
+    end			
 	
   
 end

app/admin/organisation_type.rb
@@ -3,30 +3,30 @@ ActiveAdmin.register OrganisationType do
 	 menu :priority => 4, :label => proc{I18n.t('admin.org_type')}, :parent => "Organisations management"
 
 	index do   #:organisation_id, :name
-  	column I18n.t('admin.title'), :sortable => :name do |ggn|
-        link_to ggn.name, [:admin, ggn]
-    end
-    column I18n.t('admin.desc'), :description do |descr|
-  		if !descr.description.nil? then
-  			descr.description.html_safe
-  		end
-  	end
-  	
-  	default_actions
-  end
+        column I18n.t('admin.title'), :sortable => :name do |ggn|
+            link_to ggn.name, [:admin, ggn]
+        end
+        column I18n.t('admin.desc'), :description do |descr|
+            if !descr.description.nil? then
+                descr.description.html_safe
+            end
+        end
+        
+        default_actions
+      end
   
   
-  #show organisation type details
-  show do 
+    #show organisation type details
+    show do 
 		attributes_table do
 			row :name
 			row :description do |descr|
-	  		if !descr.description.nil? then
-	  			descr.description.html_safe
-	  		end
-	  	end
-	  	row :created_at
-	  	row :updated_at
+                if !descr.description.nil? then
+                    descr.description.html_safe
+                end
+            end
+            row :created_at
+            row :updated_at
 		end
 	end
 	
@@ -34,13 +34,9 @@ ActiveAdmin.register OrganisationType do
  	sidebar I18n.t('admin.orgs'), :only => :show, :if => proc { organisation_type.organisations.count >= 1} do
 	 	table_for organisation_type.organisations.order("name asc") do |org_list|
 	 		column I18n.t('admin.org_title'), :sortable => :name do |ggn|
-        link_to ggn.name, [:admin, ggn]
-    	end
-    end
+                link_to ggn.name, [:admin, ggn]
+            end
+        end
    end
-   
-   
-   
-	
-			
+  		
 end

commit 2a5849fca297d2556838cda197d47fc7241680f1

Updated application_helper.rb to fix an issue introduced by commit 741624a15460 from DCC

app/helpers/application_helper.rb
@@ -16,12 +16,9 @@ module ApplicationHelper
 	end
 	
     
-	def link_to_add_object(name, f, association, css_class, i)
+	def link_to_add_object(name, f, association, css_class)
         new_object = f.object.class.reflect_on_association(association).klass.new
-        j = i
         fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-          j = j + 1
-          new_object.number = j
           render(association.to_s.singularize + "_fields", :f => builder)
         end
         link_to_function(name, "add_object(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => css_class)
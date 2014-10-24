2a5849fca297d2556838cda197d47fc7241680f1
Weiwei Shi <weiwei.shi@ualberta.ca>
2014-10-24 16:07:30 -0600

    Updated application_helper.rb to fix an issue introduced by commit 741624a15460 from DCC

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,12 +16,9 @@ module ApplicationHelper
 	end
 	
     
-	def link_to_add_object(name, f, association, css_class, i)
+	def link_to_add_object(name, f, association, css_class)
         new_object = f.object.class.reflect_on_association(association).klass.new
-        j = i
         fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-          j = j + 1
-          new_object.number = j
           render(association.to_s.singularize + "_fields", :f => builder)
         end
         link_to_function(name, "add_object(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => css_class)
