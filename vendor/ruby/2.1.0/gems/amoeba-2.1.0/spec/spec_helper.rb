e7711a6e907e434e0d2afe42bebc1de38577f58c
Weiwei Shi <weiwei.shi@ualberta.ca>
2014-10-31 11:45:19 -0600

    updated Gemfile, Gemfile.lock and several other gems

---
+++ vendor/ruby/2.1.0/gems/amoeba-2.1.0/spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'coveralls'
+Coveralls.wear!
+require 'amoeba'
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3",
+                                       :database => File.dirname(__FILE__) + "/test.sqlite3")
+
+load File.dirname(__FILE__) + '/support/schema.rb'
+load File.dirname(__FILE__) + '/support/models.rb'
+load File.dirname(__FILE__) + '/support/data.rb'
