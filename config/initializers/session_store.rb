544aebed932e88907d595cd07c9d1e25f8ca5a4d
Weiwei Shi <weiwei.shi@ualberta.ca>
2014-12-01 10:12:59 -0700

    changed config/initializer/session_store.rb to active_record_store as method

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-DMPonline4::Application.config.session_store :cookie_store, key: '_DMPonline4_session'
+#DMPonline4::Application.config.session_store :cookie_store, key: '_DMPonline4_session'
+DMPonline4::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
