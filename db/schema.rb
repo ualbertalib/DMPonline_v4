80f3aead97a30ae348a47c4bb62edb60b857a0d0
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-02-09 16:07:38 -0700

    update schema.rb with latest db migrate task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140707143840) do
+ActiveRecord::Schema.define(:version => 20141124224438) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -322,8 +322,17 @@ ActiveRecord::Schema.define(:version => 20140707143840) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.boolean  "published"
-
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "settings", :force => true do |t|
     t.string   "var",         :null => false
