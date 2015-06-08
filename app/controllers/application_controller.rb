df20c063ffc4a96887d423ceb0df81b4324e14f1
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-06-08 16:43:35 -0600

    merge changes that are done in deployment-portage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
- protect_from_forgery
+protect_from_forgery
+# Added settings for locales
  before_filter :set_locale
  def set_locale
  	I18n.locale = params[:locale] || I18n.default_locale
@@ -7,9 +8,9 @@ class ApplicationController < ActionController::Base
 
 # Added setting for passing local params accross pages
  def default_url_options(options = {})
-  {locale: I18n.locale }.merge options 
+	{locale: I18n.locale }.merge options
  end
-   
+  
  # Override build_footer method in ActiveAdmin::Views::Pages
   require 'active_admin_views_pages_base.rb'
   
@@ -55,4 +56,5 @@ class ApplicationController < ActionController::Base
 			@all_columns = Settings::PlanList::ALL_COLUMNS
 		end
 	end
+
 end
