6250fac1a1bc9c174f8feb3168492528570f5444
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-09-01 16:38:27 -0600

    remove project translation table, fix #41

--- db/migrate/20150623175050_create_project_translations.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjectTranslations < ActiveRecord::Migration
-  def up
-    Project.create_translation_table!({title: :string, description: :text}, {migration_data: true})
-  end
-
-  def down
-    Project.drop_translation_table! migration_data:true
-  end
-end
